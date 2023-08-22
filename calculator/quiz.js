bce9eaff575737c41e044ed87ade3e0dbcdd01d2
booyoung.lee <olivu0young@naver.com>
2023-08-22 23:53:03 +0900

    Add files via upload

---
+++ calculator/quiz.js
@@ -0,0 +1,14 @@
+function test() {
+  let result = "";
+
+  if (!a) {
+    result = "a";
+    result += "b";
+    return result;
+  }
+  if (!b) {
+    result = "c";
+  }
+  result += "b";
+  return result;
+}
